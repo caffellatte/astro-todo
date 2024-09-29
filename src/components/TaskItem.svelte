bf27be36a36263e41e605b17133a3bc8d6ff7598
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-09-29 17:08:21 +0300

    `Svelte` Integration; `TaskManager`: components, model & types
    * Update `astro.config`: add **svelte** integration (astro.config.mjs);
    * Install `@astrojs/svelte` & `svelte` packages (package.json);
    * Create `TaskItem` component (src/components/TaskItem.svelte);
    * Create `TaskManager` component (src/components/TaskManager.svelte);
    * Fix styles import at `Layout` component (src/layouts/Layout.astro);
    * Create `task` model (src/models/task.ts);
    * Update `index`: add `TaskManager` component; remove unused code (src/pages/index.astro);
    * Create `task` type (src/types/task.ts, src/types/index.ts);
    * Add `svelte.config` (svelte.config.js);
    * Update `tsconfig`: add _@/*_ path (tsconfig.json).

---
+++ src/components/TaskItem.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let task;
+
+  const { id, title, description, priority, completed, dueDate } = task;
+
+  function deleteTask(id) {
+    dispatch("deletetask", { id: id });
+  }
+
+  function updateTask(id, updatedTask) {
+    dispatch("updatetask", { id: id, updatedTask: updatedTask });
+  }
+</script>
+
+<li>
+  <strong>{title}</strong> - {description}
+  <span>({priority})</span>
+  <span>{dueDate}</span>
+  <button on:click={() => deleteTask(id)}>Delete</button>
+  <button
+    on:click={() => {
+      console.log(completed);
+      updateTask(id, { completed: !completed });
+    }}
+  >
+    {completed ? "Mark Active" : "Mark Completed"}
+  </button>
+</li>
